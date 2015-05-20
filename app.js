582e8fd670320a779189c02b9a13ef1975c771f7
wchen <sdjcw13@gmail.com>
2015-05-21 00:24:59 +0800

    增加 404 和 500 的 handler

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var path = require('path');
+var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var todos = require('./routes/todos');
 var cloud = require('./cloud');
@@ -16,9 +17,7 @@ app.use(cloud);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
-// 可以将一类的路由单独保存在一个文件中
-app.use('/todos', todos);
+app.use(cookieParser());
 
 app.get('/', function(req, res) {
   res.render('index', { currentTime: new Date() })
@@ -34,6 +33,41 @@ app.get('/1/ping', function(req, res) {
   // 可以在这里根据需要增加一些状态监测的逻辑，但检测耗时不要超过 5 秒。
   // 如果监测逻辑时异步的，则需要在回调内调用 res.send()
   res.send('pong');
+});
+
+// 可以将一类的路由单独保存在一个文件中
+app.use('/todos', todos);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+console.log(app.get('env'))
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
 module.exports = app;
