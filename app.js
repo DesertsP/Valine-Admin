297186b539495ee4bc012218096e923f3c01ad07
wchen <sdjcw13@gmail.com>
2015-05-21 15:02:04 +0800

    移除健康监测 router

--- app.js
+++ app.js
@@ -23,18 +23,6 @@ app.get('/', function(req, res) {
   res.render('index', { currentTime: new Date() })
 })
 
-/**
- * 健康监测
- * LeanEngine 会根据 `/1.1/ping` 判断应用是否正常运行。
- * 如果返回状态码为 200 则认为正常。
- * 其他状态码或者超过 5 秒没有响应则认为应用运行异常。
- */
-app.get('/1/ping', function(req, res) {
-  // 可以在这里根据需要增加一些状态监测的逻辑，但检测耗时不要超过 5 秒。
-  // 如果监测逻辑时异步的，则需要在回调内调用 res.send()
-  res.send('pong');
-});
-
 // 可以将一类的路由单独保存在一个文件中
 app.use('/todos', todos);
 
