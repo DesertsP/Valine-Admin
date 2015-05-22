1b274c416593e85a09e5fe1f520f04bf09d92c48
wchen <sdjcw13@gmail.com>
2015-05-22 16:01:59 +0800

    更新注释信息

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var cloud = require('./cloud');
 
 var app = express();
 
-// view engine setup
+// 设置 view 引擎
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
@@ -26,7 +26,8 @@ app.get('/', function(req, res) {
 // 可以将一类的路由单独保存在一个文件中
 app.use('/todos', todos);
 
-// catch 404 and forward to error handler
+// 如果任何路由都没匹配到，则认为 404
+// 生成一个异常让后面的 err handler 捕获
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
@@ -35,8 +36,7 @@ app.use(function(req, res, next) {
 
 // error handlers
 
-// development error handler
-// will print stacktrace
+// 如果是开发环境，则将异常堆栈输出到页面，方便开发调试
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
@@ -47,8 +47,7 @@ if (app.get('env') === 'development') {
   });
 }
 
-// production error handler
-// no stacktraces leaked to user
+// 如果是非开发环境，则页面只输出简单的错误信息
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
