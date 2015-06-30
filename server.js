58e304ee5fef3011b50ef9422fbd9af8ad11690e
wchen <sdjcw13@gmail.com>
2015-06-30 12:02:44 +0800

    模式使用 masterKey 权限

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ var APP_KEY = process.env.LC_APP_KEY;
 var MASTER_KEY = process.env.LC_APP_MASTER_KEY;
 
 AV.initialize(APP_ID, APP_KEY, MASTER_KEY);
+// 如果不希望使用 masterKey 权限，可以将下面一行删除
+AV.Cloud.useMasterKey();
 
 var app = require('./app');
 
