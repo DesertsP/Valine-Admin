06e94276e02ae502bbec638b9a8da33c46545ac5
wchen <sdjcw13@gmail.com>
2015-07-14 10:43:28 +0800

    增加 uncaughtException 的处理

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+'use strict';
+var domain = require('domain');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -19,9 +21,25 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+// 未处理异常捕获 middleware
+app.use(function(req, res, next) {
+  var d = domain.create();
+  d.add(req);
+  d.add(res);
+  d.on('error', function(err) {
+    console.error('uncaughtException url=%s, msg=%s', req.url, err.stack || err.message || err);
+    if(!res.finished) {
+      res.statusCode = 500;
+      res.setHeader('content-type', 'application/json; charset=UTF-8');
+      res.end('uncaughtException');
+    }
+  });
+  d.run(next);
+});
+
 app.get('/', function(req, res) {
-  res.render('index', { currentTime: new Date() })
-})
+  res.render('index', { currentTime: new Date() });
+});
 
 // 可以将一类的路由单独保存在一个文件中
 app.use('/todos', todos);
@@ -38,7 +56,7 @@ app.use(function(req, res, next) {
 
 // 如果是开发环境，则将异常堆栈输出到页面，方便开发调试
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+  app.use(function(err, req, res, next) { // jshint ignore:line
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
@@ -48,7 +66,7 @@ if (app.get('env') === 'development') {
 }
 
 // 如果是非开发环境，则页面只输出简单的错误信息
-app.use(function(err, req, res, next) {
+app.use(function(err, req, res, next) { // jshint ignore:line
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+'use strict';
 var AV = require('leanengine');
 
 var APP_ID = process.env.LC_APP_ID;
@@ -13,6 +14,6 @@ var app = require('./app');
 // 端口一定要从环境变量 `LC_APP_PORT` 中获取。
 // LeanEngine 运行时会分配端口并赋值到该变量。
 var PORT = parseInt(process.env.LC_APP_PORT || 3000);
-var server = app.listen(PORT, function () {
+app.listen(PORT, function () {
   console.log('Node app is running, port:', PORT);
 });
