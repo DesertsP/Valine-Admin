2d9b4388aba424494205aa040b9613f222a721cb
Juvenn Woo <machese@gmail.com>
2015-09-04 17:22:12 +0800

    Remove todo module

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var todos = require('./routes/todos');
 var stats = require('./routes/stats');
 var cloud = require('./cloud');
 
@@ -46,10 +45,8 @@ app.get('/dash', function(req, res) {
     res.render('dashboard');
 });
 
-app.use('/stats', stats);
-
 // 可以将一类的路由单独保存在一个文件中
-app.use('/todos', todos);
+app.use('/stats', stats);
 
 // 如果任何路由都没匹配到，则认为 404
 // 生成一个异常让后面的 err handler 捕获
