94908e54db63066b6cb4e2baabc557210ebe9799
jysperm <jysperm@gmail.com>
2016-03-04 11:09:25 +0800

    仅当没有路由发送响应时才抛出 404 异常

--- app.js
+++ app.js
@@ -49,12 +49,13 @@ app.get('/', function(req, res) {
 // 可以将一类的路由单独保存在一个文件中
 app.use('/todos', todos);
 
-// 如果任何路由都没匹配到，则认为 404
-// 生成一个异常让后面的 err handler 捕获
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+  // 如果任何一个路由都没有返回响应，则抛出一个 404 异常给后续的异常处理器
+  if (!res.headersSent) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  }
 });
 
 // error handlers
