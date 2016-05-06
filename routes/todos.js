c8d2a5262160d62888ce62ae7843797dbabfdf8b
jysperm <jysperm@gmail.com>
2016-05-06 12:11:00 +0800

    使用 Promise 接口调用 JavaScript SDK

--- routes/todos.js
+++ routes/todos.js
@@ -10,26 +10,23 @@ var Todo = AV.Object.extend('Todo');
 router.get('/', function(req, res, next) {
   var query = new AV.Query(Todo);
   query.descending('createdAt');
-  query.find({
-    success: function(results) {
+  query.find().then(function(results) {
+    res.render('todos', {
+      title: 'TODO 列表',
+      todos: results
+    });
+  }, function(err) {
+    if (err.code === 101) {
+      // 该错误的信息为：{ code: 101, message: 'Class or object doesn\'t exists.' }，说明 Todo 数据表还未创建，所以返回空的 Todo 列表。
+      // 具体的错误代码详见：https://leancloud.cn/docs/error_code.html
       res.render('todos', {
         title: 'TODO 列表',
-        todos: results
+        todos: []
       });
-    },
-    error: function(err) {
-      if (err.code === 101) {
-        // 该错误的信息为：{ code: 101, message: 'Class or object doesn\'t exists.' }，说明 Todo 数据表还未创建，所以返回空的 Todo 列表。
-        // 具体的错误代码详见：https://leancloud.cn/docs/error_code.html
-        res.render('todos', {
-          title: 'TODO 列表',
-          todos: []
-        });
-      } else {
-        next(err);
-      }
+    } else {
+      next(err);
     }
-  });
+  }).catch(next);
 });
 
 // 新增 Todo 项目
@@ -37,14 +34,9 @@ router.post('/', function(req, res, next) {
   var content = req.body.content;
   var todo = new Todo();
   todo.set('content', content);
-  todo.save(null, {
-    success: function(todo) {
-      res.redirect('/todos');
-    },
-    error: function(err) {
-      next(err);
-    }
-  });
+  todo.save().then(function(todo) {
+    res.redirect('/todos');
+  }).catch(next);
 });
 
 module.exports = router;
