5e2c94143bf7c5c0fdf609cd89a8ce86dd142c6d
wchen <sdjcw13@gmail.com>
2016-11-24 14:09:10 +0800

    chore: 增加 PORT 环境变量的使用

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var app = require('./app');
 
 // 端口一定要从环境变量 `LEANCLOUD_APP_PORT` 中获取。
 // LeanEngine 运行时会分配端口并赋值到该变量。
-var PORT = parseInt(process.env.LEANCLOUD_APP_PORT || 3000);
+var PORT = parseInt(process.env.LEANCLOUD_APP_PORT || process.env.PORT || 3000);
 app.listen(PORT, function () {
   console.log('Node app is running, port:', PORT);
 
