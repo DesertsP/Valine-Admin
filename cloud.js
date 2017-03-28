afecf74a0429b2bbe395d85a76508ae08802ad76
csun <csun@csundeMacBook-Pro.local>
2017-03-28 15:26:42 +0800

    fix hello text

--- cloud.js
+++ cloud.js
@@ -4,5 +4,5 @@ var AV = require('leanengine');
  * 一个简单的云代码方法
  */
 AV.Cloud.define('hello', function(request, response) {
-  response.success('Hello world! from test');
+  response.success('Hello world!');
 });
