6a35d7242935b873029cee8564ed42b94a44436a
jysperm <jysperm@gmail.com>
2017-04-11 18:15:38 +0800

    :arrow_up: Upgrade leanengine 2.0

--- cloud.js
+++ cloud.js
@@ -3,6 +3,6 @@ var AV = require('leanengine');
 /**
  * 一个简单的云代码方法
  */
-AV.Cloud.define('hello', function(request, response) {
-  response.success('Hello world!');
+AV.Cloud.define('hello', function(request) {
+  return 'Hello world!';
 });
