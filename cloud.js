85a962ed6830f2d1484fbf72d2596ea25aaccdc1
panjunwen <i@panjunwen.com>
2018-09-18 12:24:04 +0800

    fix bugs

--- cloud.js
+++ cloud.js
@@ -1,9 +1,8 @@
 const AV = require('leanengine');
 const mail = require('./utilities/send-mail');
+const Comment = AV.Object.extend('Comment');
 
-AV.Cloud.afterSave('Comment', function (request) {
-    let currentComment = request.object;
-
+function sendNotification(currentComment) {
     // 发送博主通知邮件
     mail.notice(currentComment);
     // AT评论通知
@@ -15,14 +14,44 @@ AV.Cloud.afterSave('Comment', function (request) {
         console.log('没有@任何人，结束!');
         return;
     }
-    // 替换掉#号，即为rid。
+    // 替换掉#号，即为rid
     let rid = h[1].replace(/#/,"");
     // 将rid存入数据库，以供管理页面使用。
     currentComment.set('rid', rid);
     let query = new AV.Query('Comment');
     query.get(rid).then(function (parentComment) {
-        mail.send(currentComment, parentComment);
+        if (parentComment.get('mail') != process.env.BLOGGER_EMAIL){
+            mail.send(currentComment, parentComment);
+        }
     }, function (error) {
         console.warn('获取@对象失败！');
     });
+}
+
+AV.Cloud.afterSave('Comment', function (request) {
+    let currentComment = request.object;
+    sendNotification(currentComment);
 });
+
+AV.Cloud.define('resend-mails', function(request) {
+    let query = new AV.Query(Comment);
+    query.greaterThanOrEqualTo('createdAt', new Date(new Date().getTime() - 24*60*60*1000));
+    // 如果你的评论量很大，可以适当调高数量限制，最高1000
+    query.limit(200);
+    return query.find().then(function(results) {
+        new Promise((resolve, reject)=>{
+            count = 0;
+            for (var i = 0; i < results.length; i++ ) {
+                if (!results[i].get('isNotified')){
+                    sendNotification(results[i]);
+                    count++;
+                }
+            }
+            resolve(count);
+        }).then((count)=>{
+            console.log(`昨日${count}条未成功发送的通知邮件处理完毕！`);
+        }).catch(()=>{
+
+        });
+    });
+  });
